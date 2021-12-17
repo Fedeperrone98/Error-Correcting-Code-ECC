65ee0707ee297c7b5a3243f7e20550e0d3238a99
Fedeperrone98 <f.perrone6@studenti.unipi.it>
2021-12-17 13:35:30 +0100

    continuo decoder

--- src/Decoder.vhd
+++ src/Decoder.vhd
@@ -1,10 +1,10 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use IEEE.numeric_std.all;
-
 
 entity Decoder is
     port( 
+        clk : in std_logic;
+        resetn : in std_logic;
         data_encoded : in std_logic_vector(15 downto 0);
         data_out : out std_logic_vector(10 downto 0);
         NE_bit : out std_logic;
@@ -14,28 +14,96 @@ entity Decoder is
 			
 end Decoder;
 
-architecture beh of Decoder is   
-    signal c1 : std_logic;
-    signal c2 : std_logic;
-    signal c4 : std_logic;
-    signal c8 : std_logic;
-    signal p : std_logic;
-    signal c : std_logic_vector(3 downto 0);
-    signal addr : integer;
+architecture beh of Decoder is
+
+    signal c_det : std_logic_vector(3 downto 0);
+    signal c_corr : std_logic_vector(3 downto 0);
+    signal p_det : std_logic;
+    signal p_corr : std_logic;
+    signal temp_out : std_logic_vector(10 downto 0);
+
+    component Error_Detect is
+        port( 
+            data_encoded : in std_logic_vector(15 downto 0);
+            c : out std_logic_vector(3 downto 0);
+            p : out std_logic
+	    );			
+    end component Error_Detect;
+
+    component Error_Correct is
+        port( 
+            data_encoded : in std_logic_vector(15 downto 0);
+            c : in std_logic_vector(3 downto 0);
+            p : in std_logic;
+            data_out : out std_logic_vector(10 downto 0)
+	    );			
+    end component Error_Correct;
+
+    component DFF_N is
+        generic( N : natural := 8);   
+        port( 
+            clk     : in std_logic;
+            a_rstn : in std_logic;
+            en      : in std_logic;
+            d       : in std_logic_vector(N - 1 downto 0);
+            q       : out std_logic_vector(N - 1 downto 0)
+        );                
+    end component DFF_N;
 
     begin
-        -- generation of control bits
-        c1 <= (data_encoded(0) xor data_encoded(2)) xor (data_encoded(4) xor data_encoded(6)) xor (data_encoded(8) xor data_encoded(10)) xor (data_encoded(12) xor data_encoded(14));
-        c2 <= (data_encoded(1) xor data_encoded(2)) xor (data_encoded(5) xor data_encoded(6)) xor (data_encoded(9) xor data_encoded(10)) xor (data_encoded(13) xor data_encoded(14));
-        c4 <= (data_encoded(3) xor data_encoded(4)) xor (data_encoded(5) xor data_encoded(6)) xor (data_encoded(11) xor data_encoded(12)) xor (data_encoded(13) xor data_encoded(14));
-        c8 <= (data_encoded(7) xor data_encoded(8)) xor (data_encoded(9) xor data_encoded(10)) xor (data_encoded(11) xor data_encoded(12)) xor (data_encoded(13) xor data_encoded(14));
+        --***************************************************************************************************************
+                                --ERROR DETECTION
+        --***************************************************************************************************************
 
-        p <= (data_encoded(0) xor data_encoded(1)) xor (data_encoded(2) xor data_encoded(3)) xor (data_encoded(4) xor data_encoded(5)) xor (data_encoded(6)) xor data_encoded(7)) xor
-        (data_encoded(8) xor data_encoded(9)) xor (data_encoded(10) xor data_encoded(11)) xor (data_encoded(12) xor data_encoded(13)) xor (data_encoded(14) xor data_encoded(15));
+        detect: Error_Detect 
+        port map(
+            data_encoded => data_encoded,
+            c => c_det,
+            p => p_det
+        );
 
-        c <= c8 & c4 & c2 & c1;
-
-        -- find the position of a possible wrong bit
-        addr <= TO_INTEGER(unsigned(c));
+        --no error (c=0 and p=0)
+        NE_bit <= '1' when c_det="0000" and p_det='0' else '0';
         
+        --single error ((c/=0 and p=1) or (c=0 and p=1))
+        SEC_bit <= '1' when c_det/="0000" and p_det='1' else 
+                    '1' when c_det="0000" and p_det='1' else '0';
+
+        --double error (c/=0 and p=0)
+        DED_bit <= '1' when c_det/="0000" and p_det='0' else '0';
+
+        reg_middle_c: DFF_N
+        generic map( N => 4)
+        port map( 
+            clk     => clk,
+            a_rstn =>  resetn,
+            en      => '1',
+            d       => c_det,
+            q       => c_corr
+        );
+
+        reg_middle_p: DFF_N
+        generic map( N => 1)
+        port map( 
+            clk     => clk,
+            a_rstn =>  resetn,
+            en      => '1',
+            d       => p_det,
+            q       => p_corr
+        );
+        
+
+        --***************************************************************************************************************
+                                --ERROR CORRECTION
+        --***************************************************************************************************************
+
+        correct: Error_correct 
+        port map(
+            data_encoded => data_encoded,
+            c => c_corr,
+            p => p_corr,
+            data_out => temp_out
+        );
+
+        data_out <= temp_out;
     end beh;
