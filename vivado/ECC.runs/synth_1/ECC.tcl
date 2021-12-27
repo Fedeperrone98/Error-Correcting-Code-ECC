5886d829c451c78e38584202ce563e5b706909d2
Fedeperrone98 <f.perrone6@studenti.unipi.it>
2021-12-27 09:52:54 +0100

    continuo sintesi

--- vivado/ECC.runs/synth_1/ECC.tcl
+++ vivado/ECC.runs/synth_1/ECC.tcl
@@ -70,9 +70,6 @@ proc create_report { reportName command } {
   }
 }
 OPTRACE "synth_1" START { ROLLUP_AUTO }
-set_param synth.incrementalSynthesisCache C:/Users/Utente/AppData/Roaming/Xilinx/Vivado/.Xil/Vivado-15424-DESKTOP-7D3FIU6/incrSyn
-set_msg_config -id {Synth 8-256} -limit 10000
-set_msg_config -id {Synth 8-638} -limit 10000
 OPTRACE "Creating in-memory project" START { }
 create_project -in_memory -part xc7z010clg400-1
 
@@ -102,6 +99,9 @@ OPTRACE "Adding files" END { }
 foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
   set_property used_in_implementation false $dcp
 }
+read_xdc {{C:/Users/Utente/Desktop/Magistrale/Electronics and communications systems/Electronic systems/Progetto/Error-Correcting-Code-ECC/vivado/ECC.srcs/constrs_ECC/new/ECC.xdc}}
+set_property used_in_implementation false [get_files {{C:/Users/Utente/Desktop/Magistrale/Electronics and communications systems/Electronic systems/Progetto/Error-Correcting-Code-ECC/vivado/ECC.srcs/constrs_ECC/new/ECC.xdc}}]
+
 set_param ips.enableIPCacheLiteLoad 1
 close [open __synthesis_is_running__ w]
 
